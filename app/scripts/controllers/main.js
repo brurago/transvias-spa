4703359e81b9f0d59ae8bb8f8fead6ba345b8e24
brurago <brurago@gmail.com>
2014-06-04 02:50:14 -0300

    projeto

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('transviasApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
