dbc6087a837d5177e120e3a7ef433b2c55e7467f
brurago <brurago@gmail.com>
2014-06-18 03:46:59 -0300

    Nova interação na busca + novo modal de seleção de login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular
-  .module('transviasApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.module('transviasApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
